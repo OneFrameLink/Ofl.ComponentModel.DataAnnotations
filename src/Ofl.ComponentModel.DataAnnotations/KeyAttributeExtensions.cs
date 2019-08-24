64977ec73b428f1de825d0d373125e61a35975dd
casperOne <casperOne@caspershouse.com>
2019-08-23 20:23:47 -0400

    Initial commit.

---
+++ src/Ofl.ComponentModel.DataAnnotations/KeyAttributeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using Ofl.Linq.Expressions;
+using Ofl.Reflection;
+
+namespace Ofl.ComponentModel.DataAnnotations
+{
+    public static class KeyAttributeExtensions
+    {
+        public static PropertyInfo GetKeyProperty<T>() =>
+            GetKeyProperty<T>(true);
+
+        public static PropertyInfo GetKeyProperty<T>(bool inherit) =>
+            typeof(T).GetKeyProperty(inherit);
+
+        public static PropertyInfo GetKeyProperty(this Type type) =>
+            type.GetKeyProperty(true);
+
+        public static PropertyInfo GetKeyProperty(this Type type, bool inherit)
+        {
+            // Validate parameters.
+            if (type == null) throw new ArgumentNullException(nameof(type));
+
+            // Get the property with the key attribute.
+            return type.GetPropertiesWithPublicInstanceGetters()
+                .SingleOrDefault(p => p.GetCustomAttribute<KeyAttribute>(inherit) != null);
+        }
+
+        public static Func<TEntity, TKey> CreateKeyPropertyGetter<TEntity, TKey>() =>
+            CreateKeyPropertyGetter<TEntity, TKey>(true);
+
+        public static Func<TEntity, TKey> CreateKeyPropertyGetter<TEntity, TKey>(bool inherit) =>
+            GetKeyProperty<TEntity>(inherit).CreateGetPropertyLambdaExpression<TEntity, TKey>().Compile();
+    }
+}
